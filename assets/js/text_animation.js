564d2bf6ef72fabb990fb3ca2127d04a7c0e5a3b
Nick Gaur <nikhilgaur581@gmail.com>
2023-08-26 06:04:46 +0530

    .

---
+++ assets/js/text_animation.js
@@ -0,0 +1,27 @@
+
+
+function tm_animate_text() {
+
+	"use strict";
+
+	var animateSpan = jQuery('.animation_text_word');
+
+	animateSpan.typed({
+		strings: [
+			"Electrifying Logistics",
+			"Sustainable Future"
+			// "Delivery",
+			//  "Marketing",
+			//   "Advertising"
+		],
+		loop: true,
+		startDelay: 1e3,
+		backDelay: 3e3
+	});
+}
+
+jQuery(document).on('ready', function () {
+	(function ($) {
+		tm_animate_text();
+	})(jQuery);
+});
